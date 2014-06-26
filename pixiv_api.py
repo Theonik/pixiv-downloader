9735f1a32c7dbf4eb26044c0b6cb04717d887af8
Theonik <theonik2006@hotmail.com>
2014-06-26 17:15:11 +0300

    -Added feature to download Ugoira frames -Added feature to download items from artist bookmarks feed -Added feature to download illustrations from a range of pages.

--- pixiv_api.py
+++ pixiv_api.py
@@ -1,5 +1,6 @@
 import urllib
 import urllib2
+import re
 import csv
 import random
 
@@ -9,7 +10,7 @@ attributes=['id', 'artist_id', 'format', 'title', 'img_folder', 'artist_name',
             'application', 'ratings', 'total_rating', 'views', 'description',
             'novel_pages', '', '', '', '', 'artist_nickname', '', '', '',
             '', 'artist_avatar_url', '']
-
+ 
 class Work:
     def __init__(self,line):
         self.line=line
@@ -20,6 +21,7 @@ class Work:
     
     def get_full_url(self,page=None):
         single_fmt="http://i{}.pixiv.net/img{}/img/{}/{}.{}"
+        ugoira_fmt="http://i{}.pixiv.net/img-zip-ugoira/img/{}/{}/{}/{}/{}/{}/{}_ugoira1920x1080.zip"
         if int(self.id)>11320000: #ID 11320066 uses new URL, 11319887 uses old URL, 11319948 uses old URL, but site thinks it uses new URL.
             novel_fmt="http://i{}.pixiv.net/img{}/img/{}/{}_big_p{}.{}"
         else:
@@ -31,7 +33,15 @@ class Work:
             work_id=self.id
             
         if page==None:
-            return single_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,self.format)
+            img_url = single_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,self.format)
+            try: #We perform a HEAD GET on the single illust file to check if it is an Ugoira. 
+                request = urllib2.Request(img_url,headers={'Referer': "http://www.pixiv.net/"})
+                request.get_method = lambda : 'HEAD'
+                response = urllib2.urlopen(request)
+            except urllib2.HTTPError as e:
+                date_arr = re.split('[- :]',self.upload_time)
+                img_url = ugoira_fmt.format(random.choice([1,2]),date_arr[0],date_arr[1],date_arr[2],date_arr[3],date_arr[4],date_arr[5],work_id)
+            return img_url
         else:
             return novel_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,page,self.format)
     
@@ -70,13 +80,45 @@ class Pixiv:
         for line in lines:
             works.append(Work(line))
         return works
-        
+    
     
     def get_works_all(self,member_id):
         page=1
         works=[]
         while True:
             current_works=self.get_works_page(member_id,page)
+            if len(current_works)==0: break
+            works.extend(current_works)
+            page+=1
+        return works
+    
+    def get_feed_page(self,page=1):
+        query={
+            'p': page,
+        }
+        data=self.make_request("iphone/bookmark_user_new_illust.php",query)
+        lines=csv.reader(data)
+        works=[]
+        for line in lines:
+            works.append(Work(line))
+        return works
+    
+    def get_feed_pages(self,start_page,end_page):
+        page=start_page
+        works=[]
+        while True:
+            current_works=self.get_feed_page(page)
+            if page>end_page: break
+            if len(current_works)==0: break
+            works.extend(current_works)
+            page+=1
+        return works
+    
+    def get_feed_all(self):
+        page=1
+        works=[]
+        while True:
+            current_works=self.get_feed_page(page)
             if len(current_works)==0: break
             works.extend(current_works)
             page+=1
