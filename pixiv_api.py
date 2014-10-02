0b863ee35a0cadb07725e3957583a4a5aa91144c
temponik <theonik2006@hotmail.com>
2014-10-02 21:00:50 +0200

    -Implemented partial fix for new URL system

--- pixiv_api.py
+++ pixiv_api.py
@@ -26,7 +26,11 @@ class Work:
     def get_full_url(self, page=None):
         single_fmt = "http://i{}.pixiv.net/img{}/img/{}/{}.{}"
         ugoira_fmt = "http://i{}.pixiv.net/img-zip-ugoira/img/{}/{}/{}/{}/{}/{}/{}_ugoira1920x1080.zip"
-        if int(self.id)>11320000: #ID 11320066 uses new URL, 11319887 uses old URL, 11319948 uses old URL, but site thinks it uses new URL.
+        if int(self.id) > 46270952:  # ID 46270953 uses new URL, 46270952 uses old URL.
+            single_fmt = "http://i{}.pixiv.net/img-original/img/{}/{}/{}/{}/{}/{}/{}_p0.{}"
+            novel_fmt = "http://i{}.pixiv.net/img-original/img/{}/{}/{}/{}/{}/{}/{}_p{}.{}"
+
+        elif int(self.id) > 11320000:  # ID 11320066 uses new URL, 11319887 uses old URL, 11319948 uses old URL, but site thinks it uses new URL.
             novel_fmt = "http://i{}.pixiv.net/img{}/img/{}/{}_big_p{}.{}"
         else:
             novel_fmt = "http://i{}.pixiv.net/img{}/img/{}/{}_p{}.{}"
@@ -35,19 +39,27 @@ class Work:
             work_id = "{}_{}".format(self.id,private_salt)
         else:
             work_id = self.id
-
+        date_arr = re.split('[- :]', self.upload_time)
         if page==None:
-            img_url = single_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,self.format)
-            try: #We perform a HEAD GET on the single illust file to check if it is an Ugoira.
+            if int(self.id) > 46270952:
+                img_url = single_fmt.format(random.choice([1,2]),date_arr[0],date_arr[1],date_arr[2],
+                                            date_arr[3],date_arr[4],date_arr[5],work_id,self.format)
+            else:
+                img_url = single_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,self.format)
+            try:  #We perform a HEAD GET on the single illust file to check if it is an Ugoira.
                 request = urllib2.Request(img_url,headers={'Referer': "http://www.pixiv.net/"})
-                request.get_method = lambda : 'HEAD'
+                request.get_method = lambda: 'HEAD'
                 response = urllib2.urlopen(request)
             except urllib2.HTTPError as e:
-                date_arr = re.split('[- :]',self.upload_time)
-                img_url = ugoira_fmt.format(random.choice([1,2]),date_arr[0],date_arr[1],date_arr[2],date_arr[3],date_arr[4],date_arr[5],work_id)
+                img_url = ugoira_fmt.format(random.choice([1,2]),date_arr[0],date_arr[1],date_arr[2],
+                                            date_arr[3],date_arr[4],date_arr[5],work_id)
             return img_url
         else:
-            return novel_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,page,self.format)
+            if int(self.id) > 46270952:
+                return novel_fmt.format(random.choice([1,2]),date_arr[0],date_arr[1],date_arr[2],
+                                        date_arr[3],date_arr[4],date_arr[5],work_id,page,self.format)
+            else:
+                return novel_fmt.format(random.choice([1,2]),self.img_folder,self.artist_nickname,work_id,page,self.format)
 
     def get_files(self):
         files = []
